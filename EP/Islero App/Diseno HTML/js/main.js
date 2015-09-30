fb55562d90472f151d7983bc145fb2ab5c59bb58
hariza17 <locoariza17@gmail.com>
2015-09-30 17:37:06 -0500

    testeando todo

---
+++ EP/Islero App/Diseno HTML/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+   $menuLeft = $('.pushmenu-left');
+   $nav_list = $('#nav_list');
+
+   $nav_list.click(function () {
+      $(this).toggleClass('active');
+      $('.pushmenu-push').toggleClass('pushmenu-push-toright');
+      $menuLeft.toggleClass('pushmenu-open');
+   });
+        
+   $("#spinner").spinner('changing', function(e, newVal, oldVal){
+    console.log(newVal);
+  });
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# HTMLEP
+este es un estilo html personalizado hecho por mi... :D
