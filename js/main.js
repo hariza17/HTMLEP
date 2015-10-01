36cb2e0c225904a46b6fd64efa413d8cfb48cbfb
hariza17 <locoariza17@gmail.com>
2015-10-01 16:01:00 -0500

    Venta canastilla terminado.... html css js / faltan los media querys para las distintas resoluciones.....

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 $(document).ready(function () {
-   $menuLeft = $('.pushmenu-left');
-   $nav_list = $('#nav_list');
 
-   $nav_list.click(function () {
-      $(this).toggleClass('active');
-      $('.pushmenu-push').toggleClass('pushmenu-push-toright');
-      $menuLeft.toggleClass('pushmenu-open');
-   });
+    //Controla el menu lateral
+    $menuLeft = $('.pushmenu-left');
+    $nav_list = $('#nav_list');
+    $nav_list.click(function () {
+        $(this).toggleClass('active');
+        $('.pushmenu-push').toggleClass('pushmenu-push-toright');
+        $menuLeft.toggleClass('pushmenu-open');
+    });
 
-   $("#spinner").spinner('changing', function(e, newVal, oldVal){
-    console.log(newVal);
-  });
+    //Controla la cantidad
+    $("#spinner").spinner('changing', function (e, newVal, oldVal) {
+        console.log(newVal);
+    });
+
+    //Oculta y muestra el carrito
+    $btn_carrito = $('.btn-carrito');
+    $('.mostrar-carrito').hide();
+    $btn_carrito.click(function () {
+        $('.cont-carrito').fadeToggle();
+    });
+
+    //Agregar productos al carrito
+
+    //MODAL FUNCIONES
+    $box=$('.box');
+    $btn_cierre=$('.btn-cierre');
+    $btn_cierre.click(function () {
+        $('.ep-modal').show();
+        $('.box').fadeIn();
+
+    });
+    $box.click(function () {
+        $('.ep-modal').fadeOut();
+        $('.box').fadeOut();
+    });
+
 });
